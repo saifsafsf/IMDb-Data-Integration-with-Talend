97371b280293c2294437149c26ecd2aa5a341764
Saif Ullah <saif110802@gmail.com>
2023-05-04 23:24:01 +0500

    Update db_movies.py

--- db_movies.py
+++ db_movies.py
@@ -3,8 +3,9 @@ from urllib.parse import quote_plus
 import pandas as pd
 
 
-password = quote_plus('$@!#isSQL11')
-engine = create_engine(f'mysql://root:{password}@localhost:3306')
+password = quote_plus('your_pass_here')
+port = 'your_port_here'
+engine = create_engine(f'mysql://root:{password}@localhost:{port}')
 conn = engine.connect()
 
 pd.read_sql(text('CREATE DATABASE movies;'), conn)
